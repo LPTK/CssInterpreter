bc79a3c0d57f1f61c9145022282bcc0536398150
a <blob@blob.com>
2013-02-07 04:08:09 +0100

    Fixing tokens priority Adding printer cases

--- src/cssInterpreter/main/CssInterpreter.java
+++ src/cssInterpreter/main/CssInterpreter.java
@@ -1,7 +1,9 @@
 package cssInterpreter.main;
 
-import java.io.InputStreamReader;
+import java.io.BufferedReader;
+import java.io.FileReader;
 import java.io.PushbackReader;
+import java.io.StringReader;
 
 import cssInterpreter.lexer.CssLexer;
 import cssInterpreter.node.Start;
@@ -18,11 +20,30 @@ public class CssInterpreter {
 		Parser parser;
 		Start ast;
 		
-		
+		String text = new String();
+		String filename = "/tmp/test.css"; // TODO
+
 		try {
-			lexer = new CssLexer(new PushbackReader(new InputStreamReader(
-					System.in), 1024));
+			FileReader sourceReader = new FileReader(filename);
+			//InputStreamReader sourceReader = new InputStreamReader(System.in);
+			BufferedReader bufferedReader = new BufferedReader(sourceReader);
+
+			StringBuilder stringBuilder = new StringBuilder();
+			int c;
+			while ((c = bufferedReader.read()) != -1) {
+				stringBuilder.append((char) c);
+			}
+
+			bufferedReader.close();
+			sourceReader.close();
+			
+			text = stringBuilder.toString();
+
+			
+			lexer = new CssLexer(new PushbackReader(new StringReader(text),
+					1024));
 			parser = new Parser(lexer);
+			
 
 			ast = parser.parse();
 
--- src/cssInterpreter/test/ASTPrinter.java
+++ src/cssInterpreter/test/ASTPrinter.java
@@ -1,15 +1,22 @@
 package cssInterpreter.test;
 
+import com.sun.corba.se.spi.ior.Identifiable;
+
 import cssInterpreter.analysis.DepthFirstAdapter;
+import cssInterpreter.node.ABaseFullPkgImport;
 import cssInterpreter.node.ABasePackageName;
+import cssInterpreter.node.AFullPackageImported;
 import cssInterpreter.node.APackageStatement;
+import cssInterpreter.node.ARecFullPkgImport;
 import cssInterpreter.node.ARecPackageName;
+import cssInterpreter.node.ATypeName;
+import cssInterpreter.node.ATypeNameImported;
 
 public class ASTPrinter extends DepthFirstAdapter {
 
 	@Override
 	public void inAPackageStatement(APackageStatement node) {
-		System.out.println("Package: ");
+		System.out.print("Package: ");
 	}
 
 	@Override
@@ -19,8 +26,49 @@ public class ASTPrinter extends DepthFirstAdapter {
 
 	@Override
 	public void inARecPackageName(ARecPackageName node) {
-		System.out.println(node.getIdent().getText() + ".");
+		System.out.print(node.getIdent().getText() + ".");
 	}
+
+	
+	@Override
+	public void inAFullPackageImported(AFullPackageImported node) {
+		System.out.print("Importing all ");
+	}
+	
+	@Override
+	public void outAFullPackageImported(AFullPackageImported node) {
+		System.out.println(" package.");
+	}
+	
+	
+
+	@Override
+	public void inABaseFullPkgImport(ABaseFullPkgImport node) {
+		System.out.print(node.getIdent().getText());
+	}
+
+	@Override
+	public void inARecFullPkgImport(ARecFullPkgImport node) {
+		System.out.print(node.getIdent().getText() + ".");
+	}
+
+	
+	
+	@Override
+	public void inATypeNameImported(ATypeNameImported node) {
+		System.out.print("Importing ");
+	}
+
+	@Override
+	public void outATypeNameImported(ATypeNameImported node) {
+		System.out.println(" type.");
+	}
+
+	@Override
+	public void inATypeName(ATypeName node) {
+		System.out.print(node.getIdent().getText());
+	}
+	
 	
 	
 
