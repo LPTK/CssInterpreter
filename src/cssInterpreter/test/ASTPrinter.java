be1ef640d02d52627859ec6c3269e796a9b688e6
LP <lionel.parreaux@gmail.com>
2013-03-08 15:23:46 +0100

    new UT

--- src/cssInterpreter/test/ASTPrinter.java
+++ src/cssInterpreter/test/ASTPrinter.java
@@ -1,9 +1,12 @@
 package cssInterpreter.test;
 
 import cssInterpreter.analysis.DepthFirstAdapter;
+import cssInterpreter.node.AAttrDecl;
 import cssInterpreter.node.AAttrDeclStatement;
 import cssInterpreter.node.AClosure;
 import cssInterpreter.node.AExprStatement;
+import cssInterpreter.node.ARefAttrType;
+import cssInterpreter.node.AValAttrType;
 
 public class ASTPrinter extends DepthFirstAdapter {
 	
@@ -23,6 +26,12 @@ public class ASTPrinter extends DepthFirstAdapter {
     public void inAAttrDeclStatement(AAttrDeclStatement node)
     {
 		System.out.println("Decl statement: "+node.toString());
+		//System.out.println((Object)(((AAttrDecl)node.getAttrDecl()).getAttrType()));
+		System.out.println((((AAttrDecl)node.getAttrDecl()).getAttrType()));
+		System.out.println((((AAttrDecl)node.getAttrDecl()).getAttrType()) instanceof AValAttrType);
+		System.out.println((((AAttrDecl)node.getAttrDecl()).getAttrType()) instanceof ARefAttrType);
+		System.out.println(((AAttrDecl)node.getAttrDecl()).getName());
+		
     }
 }
 
